c56d0a1580e1b210f0752fef49eb6ff0d6e942dc
Sibusiso Sekwane <persevierences@gmail.com>
2023-02-16 05:50:53 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
- * Return: 1.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
+	return (1);
 }
+
