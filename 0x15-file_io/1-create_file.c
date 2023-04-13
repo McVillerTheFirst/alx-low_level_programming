9fdfa149bbe2d2d47169b9c7468a8de12a592ffa
Sibusiso Sekwane <persevierences@gmail.com>
2023-04-13 00:16:19 -0700

    second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int f;
+	int nl;
+	int rw;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (f == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	for (nl = 0; text_content[nl]; nl++);
 
-	rwr = write(fd, text_content, nletters);
+	rw = write(f, text_content, nl);
 
-	if (rwr == -1)
+	if (rw == -1)
 		return (-1);
 
-	close(fd);
+	close(f);
 
 	return (1);
 }
