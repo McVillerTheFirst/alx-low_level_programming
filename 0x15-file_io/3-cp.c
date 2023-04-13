1b7168b358027973cf11645b83fb8098666d7821
Sibusiso Sekwane <persevierences@gmail.com>
2023-04-13 00:31:39 -0700

    last

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,18 +6,18 @@
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
- * Return: no return.
+ * Return: a pointer to the newly allocated buffer.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
 		exit(99);
 	}
 }
@@ -31,8 +31,8 @@ void error_file(int file_from, int file_to, char *argv[])
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	ssize_t charac, nw;
+	char buff[1024];
 
 	if (argc != 3)
 	{
@@ -44,14 +44,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	charac = 1024;
+	while (charac == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		charac = read(file_from, buff, 1024);
+		if (charac == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		nw = write(file_to, buf, charac);
+		if (nw == -1)
 			error_file(0, -1, argv);
 	}
 
